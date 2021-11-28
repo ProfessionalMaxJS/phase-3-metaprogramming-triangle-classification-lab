eb5f5fdaca306867f0f11ac6ddaef21de1a8acf0
Max <max.edison@yahoo.com>
2021-11-28 00:25:32 -0500

    commit tree aa6e2bc2b0ee1eef61ae44ac307e73f771aadcce

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  def initialize (one=0, two=0, three=0)
+    @one = one
+    @two = two
+    @three = three
+  end
+  
+  def kind
+    # binding.pry
+  if @one<=0 || @two<=0 || @three<=0
+      raise TriangleError
+  elsif @one+@two<=@three || @two+@three<=@one || @one+@three<=@two
+      raise TriangleError
+  elsif @one==@two && @two==@three
+    :equilateral
+  elsif @one==@two || @two==@three || @three==@one
+    :isosceles
+  elsif @one!=@two && @two!=@three && @three!=@one
+    :scalene
+    end
+  end
+
+
+
+  class TriangleError < StandardError
+  end
+
 end
